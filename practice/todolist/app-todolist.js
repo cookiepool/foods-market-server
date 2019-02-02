caa4323601abde29af8d1d18dfa8b9a0085dd82e
LEE <18381317534@163.com>
2019-02-02 16:32:07 +0800

    Update

---
+++ practice/todolist/app-todolist.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var todolistController = require("./controllers/todolistController");
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+todolistController(app);
+
+app.listen(3000);
+
+console.log("server is running!");
