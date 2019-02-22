bb46caeb8e02ec59a9308b2276d657b4b6cfb4d3
LEE <18381317534@163.com>
2019-02-22 16:47:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+const express = require("express");
+
+var app = express();
+
+app.listen(3000, ()=>{
+  console.log("the server is running!");
+});
+
+app.get("/index", (req, res)=>{
+  res.send("hello");
+});
+
 console.log(__dirname);
 console.log(__filename);
